0d4fab036d976db401dfeb86e7b96eff0ae18637
hbachu <harini.bachu@adtan.com>
2019-10-14 02:19:58 -0500

    clish_show

---
+++ adtn-cli-1.0/resources/raptorPlugin/ont_cli.h
@@ -0,0 +1,13 @@
+/*
+ * builtin private.h
+ *
+ */
+#ifndef RAPTORCLISHPLUGIN_H_
+#define RAPTORCLISHPLUGIN_H_
+
+#include "ClishRaptor.h"
+
+extern "C" CLISH_PLUGIN_INIT(raptor);
+extern "C" CLISH_PLUGIN_SYM(clish_show);
+
+#endif /* RAPTORCLISHPLUGIN_H_ */
---
+++ adtn-cli-1.0/resources/userExtensions/inc/ClishRaptor.h
@@ -0,0 +1,23 @@
+/**
+ * @package clish
+ * @file    ClishRaptor.h
+ * @brief   
+ */
+
+#ifndef CLISH_RAPTOR_H_
+#define CLISH_RAPTOR_H_
+
+#include "lub/c_decl.h"
+_BEGIN_C_DECL
+#include "clish/shell.h"
+#include "lub/string.h"
+#include "lub/db.h"
+_END_C_DECL
+
+int clish_show(void *clish_context,
+                 const char *script,
+                 char **out);
+
+void ClishRaptorInit(void);
+
+#endif /* CLISH_RAPTOR_H_*/
